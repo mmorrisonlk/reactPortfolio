93a1660ad4711fa6673a716a34f15d50355dfbfa
mmorrisonlk <mmorrisonlk@gmail.com>
2021-08-26 22:38:11 -0500

    Borrowed code from project to set up the structure for the header

--- src/App.js
+++ src/App.js
@@ -1,92 +1,23 @@
-import React, { Component } from "react";
-import { Card, Container, Row, Form } from 'react-bootstrap';
-import './index.css';
-import axios from "axios";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Navbar from "./components/Navbar";
+// import Saved from "./pages/Saved";
+// import NoMatch from "./pages/NoMatch";
+import { Container } from 'react-bootstrap';
 
-class App extends Component {
-  state = {
-    result: [],
-    search: "",
-    error: "",
-    rawResults: [],
-    filtered: false
-  };
-  
-  componentDidMount() {
-    axios.get('https://randomuser.me/api/?results=20')
-      .then(res => {
-        this.setState({ result:res.data.results, rawResults:res.data.results })
-        console.log(this.state.result);
-      })
-      .catch(err => console.log(err));
-  };
-
-  sortByCountry = (e) => {
-    // onClick for Button
-    if (e.target.value === "clear") {
-      this.setState({ result:this.state.rawResults });
-    } else {
-      const sorted = this.state.rawResults.slice().sort((a, b) => {
-        if (a.location.country.toLowerCase() < b.location.country.toLowerCase()) {
-          return -1;
-        } else if (a.location.country.toLowerCase() > b.location.country.toLowerCase()) {
-          return 1;
-        } else {
-          return 0;
-        }
-      })
-      this.setState({ result:sorted, rawResults:this.state.result });
-    }
-  };
-
-  filterByGender = (e) => {
-    if (e.target.value === "clear") {
-      this.setState({ result:this.state.rawResults });
-    } else {
-      const filtered = this.state.rawResults.filter(obj => obj.gender === e.target.value)
-      this.setState({ result:filtered });
-    }
-  };
-
-  render() {
-    return (
-    <div className="App">
-      <Container>
-        <Form.Group controlId="exampleForm.ControlSelect1">
-          <Form.Label>Sort by:</Form.Label>
-            <Form.Control as="select" onChange={this.sortByCountry}>
-              <option value="clear">No Sort</option>
-              <option value="country">Country</option>
-            </Form.Control>
-        </Form.Group>
-
-        <Form.Group controlId="exampleForm.ControlSelect1">
-          <Form.Label>Filter by:</Form.Label>
-            <Form.Control as="select" onChange={this.filterByGender}>
-              <option value="clear">No Filter</option>
-              <option value="male">Male</option>
-              <option value="female">Female</option>
-            </Form.Control>
-        </Form.Group>
-
-        <Row>
-          {this.state.result.map((user, idx) => (
-          <Card style={{ width: '18rem' }} key={idx}>
-          <Card.Img variant="top" src= {user.picture.large} />
-          <Card.Body>
-            <Card.Title>{user.name.first} {user.name.last}</Card.Title>
-            <Card.Text>
-              Country: {user.location.country} | Gender: {user.gender.slice(0,1).toUpperCase() + user.gender.slice(1)}
-            </Card.Text>
-          </Card.Body>
-        </Card>
-        ))}
-        </Row>
-      </Container>
-    </div>
+function App() {
+  return (
+    <Router>
+      <div>
+          <Navbar />
+          {/* <Switch>
+            <Route exact path="/saved" component={Saved} />
+            <Route exact path="/" component={Home} />            
+            <Route path="" component={NoMatch} />
+          </Switch> */}
+      </div>
+    </Router>
   );
-  }
 }
 
 export default App;
---
+++ src/components/Navbar/index.js
@@ -0,0 +1,86 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./style.css";
+import Logo from "../images/logo4.png";
+import RbNavBar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+// import NavDropdown from "react-bootstrap/NavDropdown";
+// import SearchBtn from "../SearchBtn";
+
+// Depending on the current path, this component sets the "active" class on the appropriate navigation link item
+function Navbar() {
+  return (
+    <RbNavBar
+      sticky="top"
+      collapseOnSelect
+      expand="lg"
+      bg="dark"
+      variant="dark"
+    >
+      <Link className="navbar-brand" to="/">
+        <img className="logo" alt="logo" src={Logo}></img>
+      </Link>
+      {/* <SearchBtn></SearchBtn> */}
+
+      <RbNavBar.Toggle />
+      <RbNavBar.Collapse>
+        <Nav className="mr-auto">
+          <Link
+            to="/"
+            className={
+              window.location.pathname === "/" ||
+              window.location.pathname === "/homepage"
+                ? "nav-link active"
+                : "nav-link"
+            }
+          >
+            Home
+          </Link>
+          <Link
+            to="/login"
+            className={
+              window.location.pathname === "/login"
+                ? "nav-link active"
+                : "nav-link"
+            }
+          >
+            Login/Sign Up
+          </Link>
+          <Nav>
+            <Link
+              to="/discover"
+              className={
+                window.location.pathname === "/discover"
+                  ? "nav-link active"
+                  : "nav-link"
+              }
+            >
+              Discover
+            </Link>
+            <Link
+              to="/profile"
+              className={
+                window.location.pathname === "/profile"
+                  ? "nav-link active"
+                  : "nav-link"
+              }
+            >
+              Faves
+            </Link>
+          </Nav>
+          {/* <NavDropdown
+            title="More"
+            align="end"
+            id="collasible-nav-dropdown dropdown-menu-offet"
+          > */}
+          {/* <NavDropdown.Item href="/create">Create LINEUP</NavDropdown.Item>
+            <NavDropdown.Divider /> */}
+          {/* <NavDropdown.Item href="/signup">Sign Up</NavDropdown.Item>
+          </NavDropdown> */}
+        </Nav>
+      </RbNavBar.Collapse>
+    </RbNavBar>
+  );
+}
+
+export default Navbar;
